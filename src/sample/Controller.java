5e901164f700311a2f5928a48fdc28017540584e
f0obar <dlehrmos@stud.hs-heilbronn.de>
2017-03-14 13:54:05 +0100

    added drawing pane and scramble function

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,12 +1,21 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.CheckBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 
 import java.util.Random;
 
 public class Controller {
+
+    private Line[] lines = new Line[800];
+
+    @FXML
+    private Pane drawingPane;
 
     @FXML
     private Text seedField;
@@ -24,12 +33,27 @@ public class Controller {
     private Text accesses;
 
     @FXML
+    private Pane pane;
+
+    @FXML
     void scramble() {
         if(!keepSeed.isSelected() || seedField.getText().equals("")){
             seedField.setText(""+System.currentTimeMillis());
         }
         Random generator = new Random(Long.parseLong(seedField.getText()));
 
+
+
+        for(int index = 0; index < 800; index ++){
+            drawingPane.getChildren().remove(lines[index]);
+            lines[index] = new Line(index, 800, index, 800- generator.nextInt(800));
+            lines[index].setVisible(true);
+            lines[index].setStroke(Color.BLACK);
+        }
+
+        for(Line line : lines){
+            drawingPane.getChildren().add(line);
+        }
     }
 
     @FXML
