88689e5f96abc3e853bbf948e8119423ddef6058
f0obar <dlehrmos@stud.hs-heilbronn.de>
2017-03-14 12:49:56 +0100

    General layout and methods

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,56 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.text.Text;
+
+import java.util.Random;
+
 public class Controller {
+
+    @FXML
+    private Text seedField;
+
+    @FXML
+    private CheckBox keepSeed;
+
+    @FXML
+    private Text comparisons;
+
+    @FXML
+    private Text time;
+
+    @FXML
+    private Text accesses;
+
+    @FXML
+    void scramble() {
+        if(!keepSeed.isSelected() || seedField.getText().equals("")){
+            seedField.setText(""+System.currentTimeMillis());
+        }
+        Random generator = new Random(Long.parseLong(seedField.getText()));
+
+    }
+
+    @FXML
+    void algo1() {
+
+    }
+
+    @FXML
+    void algo2() {
+
+    }
+
+    @FXML
+    void algo3() {
+
+    }
+
+    @FXML
+    void algo4() {
+
+    }
+
 }
+
