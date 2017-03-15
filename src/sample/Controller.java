fef0a7c0ccb30558fac29d1a771dff3bd3a8f92b
f0obar <dlehrmos@stud.hs-heilbronn.de>
2017-03-15 16:52:37 +0100

    Added varius random value patterns

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -35,6 +35,9 @@ public class Controller {
     private ChoiceBox<?> choiceBox;
 
     @FXML
+    private ChoiceBox<?> choiceBoxMode;
+
+    @FXML
     private Text time;
 
     @FXML
@@ -53,16 +56,89 @@ public class Controller {
         }
         Random generator = new Random(Long.parseLong(seedField.getText()));
 
+        for(Line line : lines){
+            drawingPane.getChildren().remove(line);
+        }
 
-        for(int index = 0; index < 800; index ++){
-            drawingPane.getChildren().remove(lines[index]);
-            lines[index] = new Line(index + 0.5, 800 + 0.5, index + 0.5, 800- generator.nextInt(800) + 0.5);
-            lines[index].setVisible(true);
-            lines[index].setFill(Color.BLACK);
-            lines[index].setStrokeWidth(1);
+        switch ((String)choiceBoxMode.getValue()) {
+            case "Random" : {
+                for (int index = 0; index < 800; index++) {
+                    lines[index] = new Line(index + 0.5, 800 + 0.5, index + 0.5, 800 - generator.nextInt(800) + 0.5);
+                }
+                break;
+            }
+            case "Solved" : {
+                for (int index = 0; index < 800; index++) {
+                    lines[index] = new Line(index + 0.5, 800 + 0.5, index + 0.5, 800 - index + 0.5);
+                }
+                break;
+            }
+            case "Inverted" : {
+                for (int index = 0; index < 800; index++) {
+                    lines[index] = new Line(index + 0.5, 800 + 0.5, index + 0.5, index + 0.5);
+                }
+                break;
+            }
+            case "Stair Segments" : {
+                for (int index = 0; index < 800; index+=10) {
+                    int value = generator.nextInt(790);
+                    for(int i = 0; i <10; i++){
+                        lines[index+i] = new Line(index+i + 0.5, 800 + 0.5, index+i + 0.5, 800 - value - i + 0.5);
+                    }
+                }
+                break;
+            }
+            case "Inverted Stair Segments" : {
+                for (int index = 0; index < 800; index+=10) {
+                    int value = generator.nextInt(790) + 10;
+                    for(int i = 0; i <10; i++){
+                        lines[index+i] = new Line(index+i + 0.5, 800 + 0.5, index+i + 0.5, 800 - value + i + 0.5);
+                    }
+                }
+                break;
+            }
+            case "Big Stair Segments" : {
+                for (int index = 0; index < 800; index+=100) {
+                    int value = generator.nextInt(700);
+                    for(int i = 0; i <100; i++){
+                        lines[index+i] = new Line(index+i + 0.5, 800 + 0.5, index+i + 0.5, 800 - value - i + 0.5);
+                    }
+                }
+                break;
+            }
+            case "Big Inverted Stair Segments" : {
+                for (int index = 0; index < 800; index+=100) {
+                    int value = generator.nextInt(700) + 100;
+                    for(int i = 0; i <100; i++){
+                        lines[index+i] = new Line(index+i + 0.5, 800 + 0.5, index+i + 0.5, 800 - value + i + 0.5);
+                    }
+                }
+                break;
+            }
+            case "Chunks" : {
+                for (int index = 0; index < 800; index+=10) {
+                    int value = generator.nextInt(800);
+                    for(int i = 0; i <10; i++){
+                        lines[index+i] = new Line(index+i + 0.5, 800 + 0.5, index+i + 0.5, 800 - value + 0.5);
+                    }
+                }
+                break;
+            }
+            case "Big Chunks" : {
+                for (int index = 0; index < 800; index+=100) {
+                    int value = generator.nextInt(800);
+                    for(int i = 0; i <100; i++){
+                        lines[index+i] = new Line(index+i + 0.5, 800 + 0.5, index+i + 0.5, 800 - value + 0.5);
+                    }
+                }
+                break;
+            }
         }
 
         for(Line line : lines){
+            line.setVisible(true);
+            line.setFill(Color.BLACK);
+            line.setStrokeWidth(1);
             drawingPane.getChildren().add(line);
         }
     }
@@ -153,7 +229,7 @@ public class Controller {
         do{
             sorted = true;
             //Compare
-            for(int n = 0; n<lines.length; n++){
+            for(int n = 0; n<lines.length -1; n++){
                 if(needToSwap(n,n+1)) {
                     sorted = false;
                     break;
