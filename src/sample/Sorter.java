17619db21b379d0eed343494d710f692ae81c36d
f0obar <dlehrmos@stud.hs-heilbronn.de>
2017-03-20 22:41:00 +0100

    added heap sort

--- src/sample/Sorter.java
+++ src/sample/Sorter.java
@@ -1,17 +1,14 @@
 package sample;
 
-import com.sun.javafx.tk.Toolkit;
 import javafx.application.Platform;
-import javafx.concurrent.Service;
 import javafx.concurrent.Task;
-import javafx.scene.shape.Line;
 
 import java.util.Random;
 
 /**
  * Created by Dominic on 16.03.2017.
  */
-public class Sorter extends Task{
+public class Sorter extends Task {
 
 
     public int[] values = new int[800];
@@ -20,19 +17,19 @@ public class Sorter extends Task{
     public long time = 0;
     public Controller controller;
 
-    public Sorter(Controller controller){
+    public Sorter(Controller controller) {
         this.controller = controller;
     }
 
     @Override
-    public void run(){
+    public void run() {
         Platform.runLater(() -> controller.updateComparisons(0));
         Platform.runLater(() -> controller.updateTime(0));
         Platform.runLater(() -> controller.updateAccesses(0));
 
         time = System.currentTimeMillis();
 
-        switch ((String)controller.choiceBox.getValue()){
+        switch ((String) controller.choiceBox.getValue()) {
             case "Bubble Sort1": {
                 bubbleSort1();
                 break;
@@ -46,149 +43,141 @@ public class Sorter extends Task{
                 break;
             }
             case "Quick Sort Lomuto": {
-                quickSortLomuto(0, values.length -1);
+                quickSortLomuto(0, values.length - 1);
                 break;
             }
             case "Quick Sort Hoare": {
-                quickSortHoare(0, values.length -1);
+                quickSortHoare(0, values.length - 1);
                 break;
             }
             case "Bogo Sort": {
                 bogoSort();
                 break;
             }
-            case "Insertion Sort":{
+            case "Insertion Sort": {
                 insertionSort();
                 break;
             }
-            case "Smooth Sort":{
-                insertionSort();
-                break;
-            }
-            case "Heap Sort":{
-                insertionSort();
-                break;
-            }
-            case "Merge Sort":{
-                insertionSort();
+            case "Heap Sort": {
+                heapSort();
                 break;
             }
         }
         Platform.runLater(() -> controller.updateTime(System.currentTimeMillis() - time));
     }
 
-    private void bubbleSort1(){
-        for(int n = values.length; n>1; n--){
-            for(int i=0; i<n-1;i++){
+    private void bubbleSort1() {
+        for (int n = values.length; n > 1; n--) {
+            for (int i = 0; i < n - 1; i++) {
                 comparison();
-                if(values[i] > values[i + 1]){
-                    swap(i,i+1);
+                if (values[i] > values[i + 1]) {
+                    swap(i, i + 1);
                 }
             }
         }
     }
 
-    private void bubbleSort2(){
+    private void bubbleSort2() {
         int n = values.length;
         boolean swapped;
-        do{
+        do {
             swapped = false;
-            for(int i = 0; i<n-1; i++){
+            for (int i = 0; i < n - 1; i++) {
                 comparison();
-                if(values[i] > values[i+1]){
-                    swap(i,i+1);
+                if (values[i] > values[i + 1]) {
+                    swap(i, i + 1);
                     swapped = true;
                 }
             }
-        }while (swapped);
+        } while (swapped);
     }
 
-    private void bubbleSort3(){
+    private void bubbleSort3() {
         int n = values.length;
-        do{
+        do {
             int newN = 1;
-            for(int i = 0; i<n-1; i++){
+            for (int i = 0; i < n - 1; i++) {
                 comparison();
-                if (values[i] > values[i+1]){
-                    swap(i,i+1);
-                    newN = i+1;
+                if (values[i] > values[i + 1]) {
+                    swap(i, i + 1);
+                    newN = i + 1;
                 }
             }
             n = newN;
-        }while (n>1);
+        } while (n > 1);
     }
 
-    private void quickSortHoare(int lo, int hi){
+    private void quickSortHoare(int lo, int hi) {
         comparison();
-        if(lo < hi){
+        if (lo < hi) {
             int p = partitionHoare(lo, hi);
             quickSortHoare(lo, p);
-            quickSortHoare(p+1,hi);
+            quickSortHoare(p + 1, hi);
         }
     }
 
-    private int partitionHoare(int lo, int hi){
+    private int partitionHoare(int lo, int hi) {
         int pivot = values[lo];
-        int i = lo -1;
+        int i = lo - 1;
         int j = hi + 1;
 
-        while (true){
-            do{
-                i+=1;
+        while (true) {
+            do {
+                i += 1;
                 comparison();
             } while (values[i] < pivot);
 
-            do{
-                j-=1;
+            do {
+                j -= 1;
                 comparison();
             } while (values[j] > pivot);
 
             comparison();
-            if(i >= j)
+            if (i >= j)
                 return j;
 
-            swap(i,j);
+            swap(i, j);
         }
     }
 
-    private void quickSortLomuto(int lo, int hi){
+    private void quickSortLomuto(int lo, int hi) {
         comparison();
-        if(lo < hi){
+        if (lo < hi) {
             int p = partitionHoare(lo, hi);
             quickSortHoare(lo, p - 1);
-            quickSortHoare(p+1,hi);
+            quickSortHoare(p + 1, hi);
         }
     }
 
-    private int partitionLomuto(int lo, int hi){
+    private int partitionLomuto(int lo, int hi) {
         int pivot = values[hi];
-        int i = lo -1;
-        for(int j = lo; j <= hi-1;j++){
+        int i = lo - 1;
+        for (int j = lo; j <= hi - 1; j++) {
             comparison();
-            if(values[j] < pivot){
-                i+=1;
-                swap(i,j);
+            if (values[j] < pivot) {
+                i += 1;
+                swap(i, j);
             }
         }
-        swap(i+1,hi);
-        return i+1;
+        swap(i + 1, hi);
+        return i + 1;
     }
 
-    private void bogoSort(){
+    private void bogoSort() {
         boolean sorted;
 
-        do{
+        do {
             sorted = true;
             //Compare
-            for(int i = 0; i<values.length -1; i++){
+            for (int i = 0; i < values.length - 1; i++) {
                 comparison();
-                if(values[i] < values[i+1]) {
+                if (values[i] < values[i + 1]) {
                     sorted = false;
                     break;
                 }
             }
 
-            if(!sorted){
+            if (!sorted) {
                 //Scramble
                 Random rnd = new Random(System.currentTimeMillis());
                 swap(rnd.nextInt(800), rnd.nextInt(800));
@@ -202,15 +191,15 @@ public class Sorter extends Task{
                 swap(rnd.nextInt(800), rnd.nextInt(800));
                 swap(rnd.nextInt(800), rnd.nextInt(800));
             }
-        }while (!sorted);
+        } while (!sorted);
     }
 
-    private void insertionSort(){
-        for(int i = 1; i< values.length; i++){
+    private void insertionSort() {
+        for (int i = 1; i < values.length; i++) {
             int j = i;
-            while(j > 0){
+            while (j > 0) {
                 comparison();
-                if(values[j-1] > values[j]) {
+                if (values[j - 1] > values[j]) {
                     swap(j - 1, j);
                     j -= 1;
                 } else {
@@ -219,6 +208,42 @@ public class Sorter extends Task{
             }
         }
     }
+
+
+    private void heapSort() {
+        heapSortGenerateHeap();
+        for(int i = values.length -1; i > 0; i--) {
+            swap(i,0);
+            heapSortShiftDown(0, i);
+        }
+    }
+
+    private void heapSortGenerateHeap() {
+        for(int i = (values.length / 2) - 1; i >= 0 ; i--) {
+            heapSortShiftDown(i, values.length);
+        }
+    }
+
+    private void heapSortShiftDown(int i, int n) {
+        while (i <= (n / 2) - 1) {
+            int child = ((i + 1) * 2) - 1;
+
+            if (child + 1 < n) {
+                comparison();
+                if (values[child] < values[child + 1]) {
+                    child++;
+                }
+            }
+            comparison();
+            if (values[i] < values[child]) {
+                swap(i, child);
+                i = child;
+            } else {
+                break;
+            }
+        }
+    }
+
 
 
     private void comparison(){
@@ -233,12 +258,24 @@ public class Sorter extends Task{
         while (System.nanoTime() < (delay + controller.delaySlider.getValue()*10000)){}
     }
 
+    private void set(int index, int value){
+        long delay = System.nanoTime();
+        //active waiting because of windows scheduler reasons.
+        while (System.nanoTime() < (delay + controller.delaySlider.getValue()*5000)){}
+
+        accesses+= 1;
+        Platform.runLater(() -> controller.updateAccesses(accesses));
+
+        values[index] = value;
+        Platform.runLater(() -> controller.updateLine(index, values[index]));
+    }
+
     private void swap(int index1, int index2){
         long delay = System.nanoTime();
         //active waiting because of windows scheduler reasons.
         while (System.nanoTime() < (delay + controller.delaySlider.getValue()*10000)){}
 
-        accesses+= 1;
+        accesses+= 2;
         Platform.runLater(() -> controller.updateAccesses(accesses));
 
         int tmp = values[index1];
